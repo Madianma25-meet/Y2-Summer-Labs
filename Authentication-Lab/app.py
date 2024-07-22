b58c33cf5caa8aedfc1a71eac9e731d35a316175
Madianma25-meet <Marana@gh-is.org>
2024-07-22 18:57:13 +0300

    hehe

--- Authentication-Lab/app.py
+++ Authentication-Lab/app.py
@@ -10,15 +10,16 @@ firebaseConfig = {
   "apiKey": "AIzaSyAaONmOwTHqpgworT0gg7x1AvqYeKZilOI",
   "authDomain": "authentication-lab-3f8c6.firebaseapp.com",
   "projectId": "authentication-lab-3f8c6",
-  "storageBucket": "authentication-lab-3f8c6.appspot.com",
+  "storageBucket": "authentication-lhttps://github.com/meet-projects/Y2-Summer-Labs/tree/master/0.9%20Firebase%20Realtime%20Databaseab-3f8c6.appspot.com",
   "messagingSenderId": "85167981563",
   "appId": "1:85167981563:web:fd4804e525bc93772f415e",
   "measurementId": "G-GL6E2HPHQK", 
-  "databaseURL": ""
+  "databaseURL": "https://authentication-lab-3f8c6-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
+db = firebase.database()
 
 
 
@@ -27,16 +28,23 @@ def signup():
   if request.method == "POST":
     email = request.form['email']
     password = request.form['password']
-    
-    try:
-      user = auth.create_user_with_email_and_password(email, password)
-      login_session['user'] = user
-      login_session['quotes']= []
-      login_session.modified = True
-      return redirect(url_for('home'))
-    except:
-      error = "oopsie"
-      return render_template("signup.html", error=error)
+    full_name = request.form['full_name']
+    username = request.form['username']
+
+    user_data = {"full_name": full_name,
+    "email": email,
+    "username": username
+    }
+
+    user = auth.create_user_with_email_and_password(email, password)
+    login_session['user'] = user
+    login_session['quotes']= []
+    login_session.modified = True
+
+    db.child("users").child(user['localId']).set(user_data)
+
+
+    return redirect(url_for('home'))
   return render_template('signup.html')
 
 @app.route('/signin', methods= ['GET' , 'POST'])
@@ -54,15 +62,20 @@ def signin():
 @app.route('/home', methods= ['GET' , 'POST'])
 def home():
   if request.method == "POST":
-    quote = request.form['quote']
+    text = request.form['text']
+    said_by = request.form['said_by']
+
+    quote = {"text": text,
+     "said_by": said_by,
+     "uid": login_session['user']['localId']}
+
     login_session['quotes'].append(quote)
     login_session.modified = True
+
+    db.child("Quotes").push(quote)
+
     return redirect(url_for('thanks'))
   return render_template('home.html')
-
-  # if 'user' in login_session:
-  #   return render_template('home.html', email=login_session['user']['email'])
-  # return redirect(url_for('sig'))
 
 @app.route('/signout', methods= ['GET' , 'POST'])
 def signout():
@@ -75,8 +88,8 @@ def thanks():
 
 @app.route('/display', methods= ['GET' , 'POST'])
 def display():
-  quotes = login_session['quotes']
-  return render_template('display.html', quotes=quotes)
+  
+  return render_template('display.html', quotes=db.child("Quotes").get().val())
   
 
 
